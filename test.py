978c692cf26d2f31c266889cb787cc3aa12973ef
Mahfuz Tushar <39481259+Mahfuzur15@users.noreply.github.com>
2023-05-24 22:55:50 +0600

    Update and rename new.py to test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+print("Hello, Welcome to my first Docker Project!")
+# This program adds two numbers
+
+num1 = 5
+num2 = 6
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of num1 and num2 is = ', sum)
+
+print('Next Loop Result')
+# Python List
+item = ["apple", "banana", "cherry", "orange"]
+for i in range(len(item)):
+  print(i, '=', item[i])
+
+
+
+print('Project End!')
